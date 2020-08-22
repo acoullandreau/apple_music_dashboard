7d95a535a4d7d4b8f09f094a6a4bdc7c4a853f37
Alexina Coullandreau <mozart38@gmail.com>
2020-08-22 12:01:04 +0200

    Updated gitignore

---
+++ webpage/webpack.config.js
@@ -0,0 +1,28 @@
+const WorkerPlugin = require('worker-plugin');
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index.js',
+		library: 'apple-music-dashboard'
+	},
+	module: {
+		rules: [
+		{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-env']
+				}
+			}
+		}
+		]
+	},
+	plugins: [
+		new WorkerPlugin()
+	]
+};
