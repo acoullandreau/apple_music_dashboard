7e4c31ad6db263ce3c4b291f8530d3e5283fbe27
Alexina Coullandreau <mozart38@gmail.com>
2020-08-26 11:19:04 +0200

    WIP : adding Plotly visualizations

---
+++ webpage/src/VisualizationsBuilder.js
@@ -0,0 +1,23 @@
+import React from 'react';
+//import PiePlot from './PiePlot.js'; 
+
+class VisualizationsBuilder {
+
+	static buildSkippedSongsHist() {
+		// var data = [
+		//   {
+		//     x: ['giraffes', 'orangutans', 'monkeys'],
+		//     y: [20, 14, 23],
+		//     type: 'bar'
+		//   }
+		// ];
+		// Plotly.newPlot('myDiv', data);
+	}
+
+	static buildPiePlot() {
+
+	}
+
+}
+
+export default VisualizationsBuilder;
